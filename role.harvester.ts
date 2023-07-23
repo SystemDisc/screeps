c7669a5ceac1a5382d93e531d9244a53be9b169a
Timothy Zorn <tim@zornco.com>
2023-07-22 22:42:02 -0600

    feat: spawn more harvesters

--- role.harvester.ts
+++ role.harvester.ts
@@ -1,18 +1,20 @@
 export const harvesterRun = async (room: Room) => {
   const myCreeps = room.find(FIND_MY_CREEPS);
   const harvesterCount = myCreeps.reduce((sum, creep) => {
-    return (creep.memory as any).harvester ? sum + 1 : sum;
+    return creep.name.startsWith('Harvester') ? sum + 1 : sum;
   }, 0);
+
+  const spawn = room.find(FIND_MY_SPAWNS).reduce((highestEnergySpawn, spawn) => (spawn.store[RESOURCE_ENERGY] > highestEnergySpawn.store[RESOURCE_ENERGY] ? spawn : highestEnergySpawn));
+
   if (harvesterCount === 0) {
-    const spawn = room.find(FIND_MY_SPAWNS).reduce((highestEnergySpawn, spawn) => (
-      spawn.store[RESOURCE_ENERGY] > highestEnergySpawn.store[RESOURCE_ENERGY] ? spawn : highestEnergySpawn
-    ));
     if (spawn.store[RESOURCE_ENERGY] < 300) {
       spawn.spawnCreep([WORK, CARRY, MOVE], 'Harvester1');
-    } else if (spawn.store[RESOURCE_ENERGY] == 300) {
+    } else {
       spawn.spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], 'Harvester1');
     }
-  } else {
-
+  } else if (harvesterCount < 10) {
+    if (spawn.store[RESOURCE_ENERGY] >= 300) {
+      spawn.spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], `Harvester${harvesterCount + 1}`);
+    }
   }
 };
